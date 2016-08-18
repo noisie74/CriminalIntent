2de1410dd01b93c83335470bd852baf7ceb8dc60
Michael B <michaelbor2015@gmail.com>
2016-08-18 15:08:17 -0700

    crime fragment

--- app/src/main/java/michael/com/criminalintent/CrimeActivity.java
+++ app/src/main/java/michael/com/criminalintent/CrimeActivity.java
@@ -1,13 +1,21 @@
 package michael.com.criminalintent;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+import michael.com.criminalintent.model.Crime;
+
+public class CrimeActivity extends FragmentActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+
     }
 }
